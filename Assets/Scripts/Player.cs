ca2897b3adc6302214b4acd3b24e6e15455b4a37
qiangpan chen <chenqiangpan@gmail.com>
2022-02-16 23:18:55 +1100

    add save load function

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,20 +7,43 @@ public class Player : MonoBehaviour
 
     public InventoryObject inventory;
     // Start is called before the first frame update
-  
-    public void OnTriggerEnter(Collider other) {
+
+    public void OnTriggerEnter(Collider other)
+    {
 
         var item = other.GetComponent<Item>();
-        if(item)
+        if (item)
         {
             Debug.Log("Item found");
-            inventory.AddItem(item.item,1);
+            inventory.AddItem(item.item, 1);
             Destroy(other.gameObject);
         }
-        
+
     }
 
-    private void OnApplicationQuit() {
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            inventory.Save();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            inventory.Load();
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalInput,  verticalInput,0);
+        movement.Normalize();
+        transform.Translate(movement * 6.0f * Time.deltaTime);
+    }
+
+
+
+    private void OnApplicationQuit()
+    {
         inventory.Container.Clear();
     }
 
